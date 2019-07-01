433ae76bae9080ee1c790a05a72dad59846302c5
Samuel <raseth2.cek22@gmail.com>
2019-07-01 16:14:00 -0500

    Reporte docente

--- PortafolioEPIS/Models/Tbl_Usuario.cs
+++ PortafolioEPIS/Models/Tbl_Usuario.cs
@@ -128,7 +128,7 @@ namespace PortafolioEPIS.Models
                     Password = HashHelper.SHA1(Password);
                     var usuario = db.Tbl_Usuario.Where(x => x.Nombre_Usuario == Usuario)
                                              .Where(x => x.Password_Usuario == Password)
-                                             .Where(x => x.Tbl_Docente.Codigo_CargoDocente == cargo)
+                                             .Where(x => x.Tbl_Docente.Codigo_CargoDocente == cargo )
                                              .SingleOrDefault();
                     if (usuario != null)
                     {
