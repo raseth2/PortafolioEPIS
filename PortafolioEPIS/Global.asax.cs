c1024580311194437fffc7135cc4d9d97a947544
J-Alfaro <J-Alfaro>
2019-07-02 00:59:39 +0100

    Error y algunas modficicaciones

--- PortafolioEPIS/Global.asax.cs
+++ PortafolioEPIS/Global.asax.cs
@@ -17,18 +17,18 @@ namespace PortafolioEPIS
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
 
-        //protected void Application_Error(object sender, EventArgs e)
-        //{
-        //    Exception exception = Server.GetLastError();
-        //    Response.Clear();
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Response.Clear();
 
-        //    HttpException httpException = exception as HttpException;
+            HttpException httpException = exception as HttpException;
 
-        //    int error = httpException != null ? httpException.GetHttpCode() : 0;
+            int error = httpException != null ? httpException.GetHttpCode() : 0;
 
-        //    Server.ClearError();
-        //    Response.Redirect(String.Format("~/Error/?error={0}", error, exception.Message));
-        //}
+            Server.ClearError();
+            Response.Redirect(String.Format("~/Error/?error={0}", error, exception.Message));
+        }
     }
 
 }
