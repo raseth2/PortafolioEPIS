ee4960c852baab7e7934de6726d8fd29216ed64a
Samuel <raseth2.cek22@gmail.com>
2019-07-09 04:26:16 -0500

    Prueba entrada ui ver desacargar

--- PortafolioEPIS/Controllers/Informes/PortafolioU1Controller.cs
+++ PortafolioEPIS/Controllers/Informes/PortafolioU1Controller.cs
@@ -214,6 +214,10 @@ namespace PortafolioEPIS.Controllers.Informes
             //ViewBag.ListaTbl_MedidasCorrectivas = ObjMedidadasCorrectivas.Listar();
             return View(objDetalleCargaAcademica.Obtener(id));
         }
+        public ActionResult Print(int id, string nombreCurso)
+        {
+            return new ActionAsPdf("ListaPDFPortafolioU1/" + id) { FileName = nombreCurso + ".pdf" };
+        }
         public ActionResult ExportaAPDF(int id)
         {
             return new ActionAsPdf("ListaPDFPortafolioU1/" + id);
